26ac0953dd681d05b540c59cf53db9b33d05c66e
Shiladitya <shiladitya92bose@gmail.com>
2024-03-06 11:45:01 +0530

    first commit

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill('Admin');
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('heading')).toContainText('Dashboard');
+  await page.getByRole('link', { name: 'Performance' }).click();
+  await page.locator('.oxd-select-text--after > .oxd-icon').first().click();
+  await page.getByText('Account Assistant').click();
+  await page.getByRole('button', { name: 'Search' }).click();
+  await page.getByText('No Records Found').click();
+  await page.getByRole('img', { name: 'profile picture' }).click();
+  await page.getByRole('menuitem', { name: 'Logout' }).click();
+  await expect(page.getByRole('heading')).toContainText('Login');
+
+});
